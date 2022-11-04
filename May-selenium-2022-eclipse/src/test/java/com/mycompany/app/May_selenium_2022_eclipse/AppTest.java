2797a4a3f404bd0a6e5c309a336cc1a8d79bd89a
meghanashinde888 <117448638+meghanashinde888@users.noreply.github.com>
2022-11-04 13:01:00 -0700

    Add files via upload

---
+++ May-selenium-2022-eclipse/src/test/java/com/mycompany/app/May_selenium_2022_eclipse/AppTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.app.May_selenium_2022_eclipse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
